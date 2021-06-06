34c5c562f8a12fd4c816c14d5cd74167a5e97b7f
MAJIHYE <majh0294@gmail.com>
2021-06-06 22:53:47 +0900

    First Commit [2021-06-06]

---
+++ covid19/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/header'
+import Contents from './component/contents'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Contents/>
+    </div>
+  );
+}
+
+export default App;
