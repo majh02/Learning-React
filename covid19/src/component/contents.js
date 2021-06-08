903215aff25ed928e62858161cc77ee1f43117bc
MAJIHYE <majh0294@gmail.com>
2021-06-09 00:07:06 +0900

    Create Line & Doughnut Graph [2021-06-09]

--- covid19/src/component/contents.js
+++ covid19/src/component/contents.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 const Contents = () => {
 
     const [confirmedData, setConfirmedData] = useState({})
+    const [qurantinedData, setQuarantinedData] = useState({})
+    const [comparedData, setComparedData] = useState({})
 
     useEffect(()=>{
         const fetchEvents = async() =>{
@@ -24,7 +26,7 @@ const Contents = () => {
                 const death = cur.Death;
                 const recovered = cur.Recovered;
 
-                const findItem = acc.find(a=> a.year === year && a.month === month);
+                const findItem = acc.find(a => a.year === year && a.month === month);
                 if(!findItem){
                     acc.push({
                         year, month, date, confirmed, active, death, recovered 
@@ -41,6 +43,7 @@ const Contents = () => {
                 }
                 return acc;
             }, [])
+            console.log(arr);
             
             const labels = arr.map(a => `${a.month + 1}월`);
             setConfirmedData({
@@ -54,18 +57,55 @@ const Contents = () => {
                     }
                 ]
             });
+            setQuarantinedData({
+                labels,
+                datasets: [
+                    {
+                        label: "월별 격리자 현황",
+                        borderColor: "salmon",
+                        fill: false,
+                        data: arr.map(a=>a.active)
+                    }
+                ]
+            });
+
+            const last = arr[arr.length -1];
+            setComparedData({
+                label: ["확진자", "격리해제","사망"],
+                datasets: [
+                    {
+                        label: "누적, 확진, 해제, 사망 비율",
+                        backgroundColor: ["#ff3d67", "#059bff", "#ffc233"],
+                        borderColor: ["#ff3d67", "#059bff", "#ffc233"],
+                        fill: false,
+                        data: [last.confirmed, last.recovered, last.death]
+                    }
+                ]
+            });
         }
         fetchEvents()
     })
-
+    
     return (
         <section>
             <h2>국내 코로나 현황</h2>
             <div className="contents">
                 <div>
                     <Bar data = {confirmedData} options={
-                        {title:{display:true, text: "누적 확진자 추이", fontSize: 16}},
-                        {legend:{display:true, position: "bottom"}}
+                        {title: {display:true, text: "누적 확진자 추이", fontSize: 16}},
+                        {legend: {display:true, position: "bottom"}}
+                    }/>
+                </div>
+                <div>
+                    <Line data = {qurantinedData} options={
+                        {title: {display:true, text: "월별 격리자 현황", fontSize: 16}},
+                        {legend: {display:true, position: "bottom"}}
+                    }/>
+                </div>
+                <div>
+                    <Doughnut data = {comparedData} options={
+                        {title: {display:true, text: `누적, 확진, 해제, 사망 (${new Date().getMonth()+1}월)`, fontSize: 16}},
+                        {legend: {display:true, position: "bottom"}}
                     }/>
                 </div>
             </div>
